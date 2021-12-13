5d1e0cab86821eef867b265cc90f5a32ded102f8
Adrian Redosz <adrian.redosz@allegro.pl>
2021-12-13 12:53:53 +0100

    little changes

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -9,12 +9,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @SpringBootApplication
 @RestController
 public class Application {
-
-  private String prevAction;
 
   static class Self {
     public String href;
@@ -61,6 +62,70 @@ public class Application {
     System.out.println(arenaUpdate);
 
     String myPlayer = getMyPlayer(arenaUpdate);
+    int width = arenaUpdate.arena.dims.get(0);
+    int height = arenaUpdate.arena.dims.get(1);
+
+    String[][] otherPlayersMap = new String[width][height];
+    List<String> collectedPlayers = arenaUpdate.arena.state.entrySet().stream()
+            .filter(player -> !player.getKey().equals(myPlayer))
+            .map(player -> {
+              int playerX = player.getValue().x;
+              int playerY = player.getValue().y;
+              otherPlayersMap[playerX][playerY] = "X";
+              return player.getKey();
+            })
+            .collect(toList());
+
+    PlayerState myPlayerState = arenaUpdate.arena.state.entrySet().stream()
+            .filter(player -> player.getKey().equals(myPlayer))
+            .map(Map.Entry::getValue)
+            .findFirst()
+            .orElse(null);
+
+    if (myPlayerState == null) {
+      return "T";
+    }
+
+    int myX = myPlayerState.x;
+    int myY = myPlayerState.y;
+    String myDirection = myPlayerState.direction;
+
+    boolean canThrow = false;
+    if (myDirection == "N") {
+      int checkY_1 = myY - 1;
+      int checkY_2 = myY - 2;
+      int checkY_3 = myY - 3;
+      canThrow = (checkY_1 >= 0 && checkY_1 < height && otherPlayersMap[myX][checkY_1].equals("X"))
+              || (checkY_2 >= 0 && checkY_2 < height && otherPlayersMap[myX][checkY_2].equals("X"))
+              || (checkY_3 >= 0 && checkY_3 < height && otherPlayersMap[myX][checkY_3].equals("X"));
+
+    } else if (myDirection == "S") {
+      int checkY_1 = myY + 1;
+      int checkY_2 = myY + 2;
+      int checkY_3 = myY + 3;
+      canThrow = (checkY_1 >= 0 && checkY_1 < height && otherPlayersMap[myX][checkY_1].equals("X"))
+              || (checkY_2 >= 0 && checkY_2 < height && otherPlayersMap[myX][checkY_2].equals("X"))
+              || (checkY_3 >= 0 && checkY_3 < height && otherPlayersMap[myX][checkY_3].equals("X"));
+
+    } else if (myDirection == "W") {
+      int checkX_1 = myY - 1;
+      int checkX_2 = myY - 2;
+      int checkX_3 = myY - 3;
+      canThrow = (checkX_1 >= 0 && checkX_1 < width && otherPlayersMap[checkX_1][myY].equals("X"))
+              || (checkX_2 >= 0 && checkX_2 < width && otherPlayersMap[checkX_2][myY].equals("X"))
+              || (checkX_3 >= 0 && checkX_3 < width && otherPlayersMap[checkX_3][myY].equals("X"));
+    } else if (myDirection == "E") {
+      int checkX_1 = myY + 1;
+      int checkX_2 = myY + 2;
+      int checkX_3 = myY + 3;
+      canThrow = (checkX_1 >= 0 && checkX_1 < width && otherPlayersMap[checkX_1][myY].equals("X"))
+              || (checkX_2 >= 0 && checkX_2 < width && otherPlayersMap[checkX_2][myY].equals("X"))
+              || (checkX_3 >= 0 && checkX_3 < width && otherPlayersMap[checkX_3][myY].equals("X"));
+    }
+
+    if (canThrow) {
+      return "T";
+    }
 
     String currentAction = getNextAction();
 
@@ -72,22 +137,9 @@ public class Application {
   }
 
   private String getNextAction() {
-    String[] commands = new String[]{"F", "R", "L", "T"};
-    int i = new Random().nextInt(4);
-    String currentAction = commands[i];
-
-    if (prevAction != "T" && currentAction != "T") {
-      return "T";
-    }
-
-    if (prevAction == "T" && currentAction == "T") {
-      i = new Random().nextInt(4);
-      currentAction = commands[i];
-    }
-
-    prevAction = currentAction;
-
-    return currentAction;
+    String[] commands = new String[]{"F", "R", "L"};
+    int i = new Random().nextInt(3);
+    return commands[i];
   }
 
 }
